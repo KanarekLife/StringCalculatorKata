deaf52166d05a1a0473a455e4b7b7f06dc392ef5
Stanisław Nieradko <stanislaw@nieradko.com>
2022-04-13 14:29:32 +0200

    refactor: Refactored GetDelimiters() in StringCalculator

--- src/StringCalculator.cs
+++ src/StringCalculator.cs
@@ -4,59 +4,59 @@ namespace StringCalculatorKata;
 
 public class StringCalculator : IStringCalculator
 {
+    private string[] GetDelimiters(string numbers, out int pos)
+    {
+        if (numbers.StartsWith("//"))
+        {
+            var delimiters = new List<string>();
+            var anyBracketOpened = false;
+            var currentDelimiter = new StringBuilder();
+            for (var i = 2; i < numbers.Length; i++)
+            {
+                var cur = numbers[i];
+                switch (cur)
+                {
+                    case '\n':
+                    {
+                        pos = i+1;
+                        if (!anyBracketOpened)
+                        {
+                            return new[] { currentDelimiter.ToString() };
+                        }
+                        return delimiters.ToArray();
+                    }
+                    case '[':
+                        anyBracketOpened = true;
+                        break;
+                    case ']':
+                        delimiters.Add(currentDelimiter.ToString());
+                        currentDelimiter.Clear();
+                        break;
+                    default:
+                        currentDelimiter.Append(cur);
+                        break;
+                }
+            }
+
+            throw new Exception("Delimiter header should be ended with a newline symbol");
+        }
+
+        pos = 0;
+        return new [] {",","\n"};
+    }
     public int Add(string numbers)
     {
         if (numbers == string.Empty)
         {
             return 0;
         }
-
-        var lines = numbers.Split('\n');
-        var delimiters = new List<string> { ",", "\n" };
         
-        if (lines[0].StartsWith("//"))
-        {
-            var header = lines[0];
-            delimiters = new List<string>();
-            var openBracket = true;
-            var anyBracketOpened = false;
-            var delimiterBuilder = new StringBuilder();
-            for (var i = 2; i < header.Length; i++)
-            {
-                switch (header[i])
-                {
-                    case '[':
-                        openBracket = true;
-                        anyBracketOpened = true;
-                        break;
-                    case ']':
-                        openBracket = false;
-                        delimiters.Add(delimiterBuilder.ToString());
-                        delimiterBuilder.Clear();
-                        break;
-                    default:
-                    {
-                        if (openBracket)
-                        {
-                            delimiterBuilder.Append(header[i]);
-                        }
+        var delimiters = GetDelimiters(numbers, out int pos);
 
-                        break;
-                    }
-                }
-            }
-
-            if (!anyBracketOpened)
-            {
-                delimiters.Add(delimiterBuilder.ToString());
-            }
-            lines = lines[1..];
-        }
-
-        var values = lines
-            .Aggregate((a, b) => $"{a}\n{b}")
+        var values = numbers[pos..]
             .Split(delimiters.ToArray(), StringSplitOptions.TrimEntries)
             .Select(str => Convert.ToInt32(str))
+            .Where(value => value < 1001)
             .ToArray();
 
         var negativeValues = values.Where(value => value < 0).ToArray();
@@ -67,7 +67,6 @@ public class StringCalculator : IStringCalculator
         }
 
         return values
-            .Where(value => value < 1001)
             .Sum();
     }
 }
